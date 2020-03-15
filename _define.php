3a3c8d6b6422fa3d15459ca65418717065b1681d
padawan83 <philippe@dissitou.org>
2020-03-15 09:50:05 +0100

    Initial commit

---
+++ _define.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @brief Resume, a theme for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Themes
+ *
+ * @author Start Bootstrap and Philippe aka amalgame
+ *
+ * @copyright Philippe Hénaff philippe@dissitou.org
+ * @copyright GPL-2.0
+ */
+
+if (!defined('DC_RC_PATH')) {
+    return;
+}
+
+$this->registerModule(
+    "Resume",                                           // Name
+    "A simple Bootstrap blog theme",                    // Description
+    "Start Bootstrap and Philippe",                     // Author
+    '1.0',                                              // Version
+    [                                                   // Properties
+        'requires'          => [['core', '2.16']], 		// Dependencies
+        'standalone_config' => true,
+        'type'              => 'theme',
+        'tplset' => 'dotty'
+    ]
+);
---
+++ _public.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * @brief Resume, a theme for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Themes
+ *
+ * @author Start Bootstrap and Philippe aka amalgame
+ *
+ * @copyright Philippe Hénaff philippe@dissitou.org
+ * @copyright GPL-2.0
+ */
+
+if (!defined('DC_RC_PATH')) {
+    return;
+}
+
+# Simple menu template functions
+$core->tpl->addValue('ResumeSimpleMenu', ['tplResumeSimpleMenu', 'resumeSimpleMenu']);
+
+class tplResumeSimpleMenu
+{
+    # Template function
+    public static function resumeSimpleMenu($attr)
+    {
+        global $core;
+
+        if (!(boolean) $core->blog->settings->system->simpleMenu_active) {
+            return '';
+        }
+
+        $class       = isset($attr['class']) ? trim($attr['class']) : '';
+        $id          = isset($attr['id']) ? trim($attr['id']) : '';
+        $description = isset($attr['description']) ? trim($attr['description']) : '';
+
+        if (!preg_match('#^(title|span|both|none)$#', $description)) {
+            $description = '';
+        }
+
+        return '<?php echo tplResumeSimpleMenu::displayMenu(' .
+        "'" . addslashes($class) . "'," .
+        "'" . addslashes($id) . "'," .
+        "'" . addslashes($description) . "'" .
+            '); ?>';
+    }
+
+    public static function displayMenu($class = '', $id = '', $description = '')
+    {
+        global $core;
+
+        $ret = '';
+
+        if (!(boolean) $core->blog->settings->system->simpleMenu_active) {
+            return $ret;
+        }
+
+        $menu = $core->blog->settings->system->simpleMenu;
+        if (is_array($menu)) {
+            // Current relative URL
+            $url     = $_SERVER['REQUEST_URI'];
+            $abs_url = http::getHost() . $url;
+
+            // Home recognition var
+            $home_url       = html::stripHostURL($core->blog->url);
+            $home_directory = dirname($home_url);
+            if ($home_directory != '/') {
+                $home_directory = $home_directory . '/';
+            }
+
+            // Menu items loop
+            foreach ($menu as $i => $m) {
+                # $href = lien de l'item de menu
+                $href = $m['url'];
+                $href = html::escapeHTML($href);
+
+                # Cope with request only URL (ie ?query_part)
+                $href_part = '';
+                if ($href != '' && substr($href, 0, 1) == '?') {
+                    $href_part = substr($href, 1);
+                }
+
+                $targetBlank = ((isset($m['targetBlank'])) && ($m['targetBlank'])) ? true : false;
+
+                # Active item test
+                $active = false;
+                if (($url == $href) ||
+                    ($abs_url == $href) ||
+                    ($_SERVER['URL_REQUEST_PART'] == $href) ||
+                    (($href_part != '') && ($_SERVER['URL_REQUEST_PART'] == $href_part)) ||
+                    (($_SERVER['URL_REQUEST_PART'] == '') && (($href == $home_url) || ($href == $home_directory)))) {
+                    $active = true;
+                }
+                $title = $span = '';
+
+                if ($m['descr']) {
+                    if (($description == 'title' || $description == 'both') && $targetBlank) {
+                        $title = html::escapeHTML(__($m['descr'])) . ' (' .
+                        __('new window') . ')';
+                    } elseif ($description == 'title' || $description == 'both') {
+                        $title = html::escapeHTML(__($m['descr']));
+                    }
+                    if ($description == 'span' || $description == 'both') {
+                        $span = ' <span class="simple-menu-descr">' . html::escapeHTML(__($m['descr'])) . '</span>';
+                    }
+                }
+
+                if (empty($title) && $targetBlank) {
+                    $title = __('new window');
+                }
+                if ($active && !$targetBlank) {
+                    $title = (empty($title) ? __('Active page') : $title . ' (' . __('active page') . ')');
+                }
+
+                $label = html::escapeHTML(__($m['label']));
+
+                $item = new ArrayObject([
+                    'url'    => $href,   // URL
+                    'label'  => $label,  // <a> link label
+                    'title'  => $title,  // <a> link title (optional)
+                    'span'   => $span,   // description (will be displayed after <a> link)
+                    'active' => $active, // status (true/false)
+                    'class'  => ''      // additional <li> class (optional)
+                ]);
+
+                # --BEHAVIOR-- publicSimpleMenuItem
+                $core->callBehavior('publicSimpleMenuItem', $i, $item);
+
+                $ret .= '<li class="nav-item li' . ($i + 1) .
+                    ($item['active'] ? ' active' : '') .
+                    ($i == 0 ? ' li-first' : '') .
+                    ($i == count($menu) - 1 ? ' li-last' : '') .
+                    ($item['class'] ? ' ' . $item['class'] : '') .
+                    '">' .
+                    '<a class="nav-link js-scroll-trigger" href="' . $href . '"' .
+                    (!empty($item['title']) ? ' title="'. $label . ' - ' . $item['title'] . '"' : '') .
+                    (($targetBlank) ? ' target="_blank" rel="noopener noreferrer"' : '') . '>' .
+                    '<span class="simple-menu-label">' . $item['label'] . '</span>' .
+                    $item['span'] . '</a>' .
+                    '</li>';
+            }
+            // Final rendering
+            if ($ret) {
+                $ret = '<ul ' . ($id ? 'id="' . $id . '"' : '') . ' class="simple-menu' . ($class ? ' ' . $class : '') . '">' . "\n" . $ret . "\n" . '</ul>';
+            }
+        }
+
+        return $ret;
+    }
+}
