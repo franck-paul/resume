c26563a5ec9c8dc6f44483557993cf1749fa0710
Philippe-dev <philippe@dissitou.org>
2020-04-15 08:39:24 +0200

    Update _define.php and publish new version

--- _define.php
+++ _define.php
@@ -19,7 +19,7 @@ $this->registerModule(
     "Resume",                                           // Name
     "A simple Bootstrap blog theme",                    // Description
     "Start Bootstrap and Philippe aka amalgame",        // Author
-    '1.4',                                              // Version
+    '1.5',                                              // Version
     [                                                   // Properties
         'requires'          => [['core', '2.16']], 		// Dependencies
         'standalone_config' => true,
