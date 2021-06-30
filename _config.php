f7ba9e7f0b50fc077ab8aa660f1d700cc21c1d1e
Philippe-dev <philippe@dissitou.org>
2021-06-30 12:02:19 +0200

    Social links config WIP

--- _config.php
+++ _config.php
@@ -39,23 +39,81 @@ if (!isset($s['main_color'])) {
     $s['main_color'] = '#bd5d38';
 }
 
+$stickers = $core->blog->settings->themes->get($core->blog->settings->system->theme . '_stickers');
+$stickers = @unserialize($stickers);
+
+$stickers_full = [];
+// Get all sticker images already used
+if (is_array($stickers)) {
+    foreach ($stickers as $v) {
+        $stickers_full[] = $v['image'];
+    }
+}
+// Get social media images
+$stickers_images = ['fab fa-diaspora','fas fa-rss','fab fa-linkedin-in','fab fa-gitlab','fab fa-github','fab fa-twitter','fab fa-facebook-f',
+'fab fa-instagram', 'fab fa-mastodon','fab fa-pinterest','fab fa-snapchat','fab fa-soundcloud','fab fa-youtube'];
+if (is_array($stickers_images)) {
+    foreach ($stickers_images as $v) {
+        if (!in_array($v, $stickers_full)) {
+            // image not already used
+            $stickers[] = [
+                    'label' => null,
+                    'url'   => null,
+                    'image' => $v];
+        }
+    }
+}
+
 // Load contextual help
 if (file_exists(dirname(__FILE__) . '/locales/' . $_lang . '/resources.php')) {
     require dirname(__FILE__) . '/locales/' . $_lang . '/resources.php';
 }
 
+$conf_tab = $_POST['conf_tab'] ?? 'html';
+
 if (!empty($_POST)) {
     try {
         # HTML
-        if (!empty($_POST['resume_user_image'])) {
-            $s['resume_user_image'] = $_POST['resume_user_image'];
-        } else {
-            $s['resume_user_image'] = $resume_default_image_url;
+        if ($conf_tab == 'css') {
+            if (!empty($_POST['resume_user_image'])) {
+                $s['resume_user_image'] = $_POST['resume_user_image'];
+            } else {
+                $s['resume_user_image'] = $resume_default_image_url;
+            }
+            $s['main_color'] = $_POST['main_color'];
         }
-        $s['main_color'] = $_POST['main_color'];
-        
+
+        if ($conf_tab == 'html') {
+            $stickers = [];
+            for ($i = 0; $i < count($_POST['sticker_image']); $i++) {
+                $stickers[] = [
+                    'label' => $_POST['sticker_label'][$i],
+                    'url'   => $_POST['sticker_url'][$i],
+                    'image' => $_POST['sticker_image'][$i]
+                ];
+            }
+
+            $order = [];
+            if (empty($_POST['ds_order']) && !empty($_POST['order'])) {
+                $order = $_POST['order'];
+                asort($order);
+                $order = array_keys($order);
+            }
+            if (!empty($order)) {
+                $new_magalogue_stickers = [];
+                foreach ($order as $i => $k) {
+                    $new_magalogue_stickers[] = [
+                        'label' => $stickers[$k]['label'],
+                        'url'   => $stickers[$k]['url'],
+                        'image' => $stickers[$k]['image']
+                    ];
+                }
+                $stickers = $new_magalogue_stickers;
+            }
+        }
         $core->blog->settings->addNamespace('themes');
         $core->blog->settings->themes->put($core->blog->settings->system->theme . '_style', serialize($s));
+        $core->blog->settings->themes->put($core->blog->settings->system->theme . '_stickers', serialize($stickers));
 
         // Blog refresh
         $core->blog->triggerBlog();
@@ -73,6 +131,8 @@ if (!empty($_POST)) {
 if (!$standalone_config) {
     echo '</form>';
 }
+
+echo '<div class="multi-part" id="themes-list' . ($conf_tab == 'html' ? '' : '-html') . '" title="' . __('Presentation') . '">';
 
 echo '<form id="theme_config" action="' . $core->adminurl->get('admin.blog.theme', ['conf' => '1']) .
     '" method="post" enctype="multipart/form-data">';
@@ -95,15 +155,61 @@ echo '<p class="hidden-if-js">' . form::field('resume_user_image', 30, 255, $s['
 echo '</div>';
 
 echo '<h4 class="pretty-title">' . __('Colors') . '</h4>';
-
 echo '<p class="field maximal"><label for="main_color">' . __('Main color:') . '</label> ' .
     form::color('main_color', 30, 255, $s['main_color']) . '</p>' ;
 
+echo '<p><input type="hidden" name="conf_tab" value="css" /></p>';
 echo '<p class="clear"><input type="submit" value="' . __('Save') . '" />' . $core->formNonce() . '</p>';
 echo form::hidden(['theme-url'], $theme_url);
 echo '</form>';
 
+echo '</div>'; // Close tab
 
+echo '<div class="multi-part" id="themes-list' . ($conf_tab == 'css' ? '' : '-css') . '" title="' . __('Links') . '">';
+echo '<form id="theme_config" action="' . $core->adminurl->get('admin.blog.theme', ['conf' => '1']) .
+    '" method="post" enctype="multipart/form-data">';
+
+echo '<h4 class="pretty-title">' . __('Social links') . '</h4>';
+
+echo
+'<div class="table-outer">' .
+'<table class="dragable">' . '<caption class="sr-only">' . __('Social links (header)') . '</caption>' .
+'<thead>' .
+'<tr>' .
+'<th scope="col">' . '</th>' .
+'<th scope="col">' . __('Image') . '</th>' .
+'<th scope="col">' . __('Label') . '</th>' .
+'<th scope="col">' . __('URL') . '</th>' .
+    '</tr>' .
+    '</thead>' .
+    '<tbody id="stickerslist">';
+$count = 0;
+foreach ($stickers as $i => $v) {
+    $count++;
+    $v['service'] = str_replace('-link.png', '', $v['image']);
+    echo
+    '<tr class="line" id="l_' . $i . '">' .
+    '<td class="handle">' . form::number(['order[' . $i . ']'], [
+        'min'     => 0,
+        'max'     => count($stickers),
+        'default' => $count,
+        'class'   => 'position'
+    ]) .
+    form::hidden(['dynorder[]', 'dynorder-' . $i], $i) . '</td>' .
+    '<td class="linkimg">' . form::hidden(['sticker_image[]'], $v['image']) . '<i class="' . $v['image'] . '"></i> ' . '</td>' .
+    '<td scope="row">' . form::field(['sticker_label[]', 'dsl-' . $i], 20, 255, $v['label']) . '</td>' .
+    '<td>' . form::field(['sticker_url[]', 'dsu-' . $i], 40, 255, $v['url']) . '</td>' .
+        '</tr>';
+}
+echo
+    '</tbody>' .
+    '</table></div>';
+
+    echo '<p><input type="hidden" name="conf_tab" value="html" /></p>';
+    echo '<p class="clear">' . form::hidden('ds_order', '') . '<input type="submit" value="' . __('Save') . '" />' . $core->formNonce() . '</p>';
+    echo '</form>';
+
+echo '</div>'; // Close tab
 dcPage::helpBlock('resume');
 
 // Legacy mode
--- _prepend.php
+++ _prepend.php
@@ -24,6 +24,7 @@ if (!defined('DC_CONTEXT_ADMIN')) {
 # Behaviors
 $GLOBALS['core']->addBehavior('adminPageHTMLHead', [__NAMESPACE__ . '\tplResumeThemeAdmin', 'adminPageHTMLHead']);
 $GLOBALS['core']->addBehavior('adminPopupMedia', [__NAMESPACE__ . '\tplResumeThemeAdmin', 'adminPopupMedia']);
+$GLOBALS['core']->addBehavior('adminPageHTTPHeaderCSP', [__NAMESPACE__ . '\tplResumeThemeAdmin','adminPageHTTPHeaderCSP']);
 
 class tplResumeThemeAdmin
 {
@@ -41,7 +42,44 @@ class tplResumeThemeAdmin
         }
 
         echo '<script src="' . $theme_url . '/js/admin.js' . '"></script>'."\n".
-       '<link rel="stylesheet" media="screen" href="' . $theme_url . '/css/admin.css'. '" />'."\n";
+        '<script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>'."\n".
+        '<link rel="stylesheet" media="screen" href="' . $theme_url . '/css/admin.css'. '" />'."\n";
+        
+        echo "\n" . '<!-- Header directives for Magalogue configuration -->' . "\n";
+        $core->auth->user_prefs->addWorkspace('accessibility');
+        if (!$core->auth->user_prefs->accessibility->nodragdrop) {
+            echo
+            \dcPage::jsLoad('js/jquery/jquery-ui.custom.js') .
+            \dcPage::jsLoad('js/jquery/jquery.ui.touch-punch.js');
+            echo <<<EOT
+<script>
+$(function() {
+    $('#stickerslist').sortable({'cursor':'move'});
+    $('#stickerslist tr').hover(function () {
+        $(this).css({'cursor':'move'});
+    }, function () {
+        $(this).css({'cursor':'auto'});
+    });
+    $('#theme_config').submit(function() {
+        var order=[];
+        $('#stickerslist tr td input.position').each(function() {
+            order.push(this.name.replace(/^order\[([^\]]+)\]$/,'$1'));
+        });
+        $('input[name=ds_order]')[0].value = order.join(',');
+        return true;
+    });
+    $('#stickerslist tr td input.position').hide();
+    $('#stickerslist tr td.handle').addClass('handler');
+});
+</script>
+<style>
+.linkimg img {
+    padding: 3px;
+    background-color: #fff;
+}
+</style>
+EOT;
+        }
     }
     
     public static function adminPopupMedia($editor = '')
@@ -58,5 +96,14 @@ class tplResumeThemeAdmin
         }
 
         return '<script src="' . $theme_url . '/js/popup_media.js' . '"></script>';
+    }
+
+    public static function adminPageHTTPHeaderCSP($csp)
+    {
+        if (isset($csp['script-src'])) {
+            $csp['script-src'] .= ' use.fontawesome.com';
+        } else {
+            $csp['script-src'] = 'use.fontawesome.com';
+        }
     }
 }
