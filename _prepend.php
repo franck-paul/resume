b9613e6fc7899692188385082e0bb9b57a04876e
Philippe-dev <philippe@dissitou.org>
2021-06-30 17:26:19 +0200

    better js load

--- _prepend.php
+++ _prepend.php
@@ -45,40 +45,11 @@ class tplResumeThemeAdmin
         '<script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>'."\n".
         '<link rel="stylesheet" media="screen" href="' . $theme_url . '/css/admin.css'. '" />'."\n";
         
-        echo "\n" . '<!-- Header directives for Magalogue configuration -->' . "\n";
         $core->auth->user_prefs->addWorkspace('accessibility');
         if (!$core->auth->user_prefs->accessibility->nodragdrop) {
             echo
             \dcPage::jsLoad('js/jquery/jquery-ui.custom.js') .
             \dcPage::jsLoad('js/jquery/jquery.ui.touch-punch.js');
-            echo <<<EOT
-<script>
-$(function() {
-    $('#stickerslist').sortable({'cursor':'move'});
-    $('#stickerslist tr').hover(function () {
-        $(this).css({'cursor':'move'});
-    }, function () {
-        $(this).css({'cursor':'auto'});
-    });
-    $('#theme_config').submit(function() {
-        var order=[];
-        $('#stickerslist tr td input.position').each(function() {
-            order.push(this.name.replace(/^order\[([^\]]+)\]$/,'$1'));
-        });
-        $('input[name=ds_order]')[0].value = order.join(',');
-        return true;
-    });
-    $('#stickerslist tr td input.position').hide();
-    $('#stickerslist tr td.handle').addClass('handler');
-});
-</script>
-<style>
-.linkimg img {
-    padding: 3px;
-    background-color: #fff;
-}
-</style>
-EOT;
         }
     }
     
--- js/admin.js
+++ js/admin.js
@@ -26,4 +26,22 @@ $(function () {
             $('#resume_user_image_src').attr('alt', src);
         }
     });
+
+    // stickers reorder
+    $('#stickerslist').sortable({'cursor':'move'});
+    $('#stickerslist tr').hover(function () {
+        $(this).css({'cursor':'move'});
+    }, function () {
+        $(this).css({'cursor':'auto'});
+    });
+    $('#theme_config').submit(function() {
+        var order=[];
+        $('#stickerslist tr td input.position').each(function() {
+            order.push(this.name.replace(/^order\[([^\]]+)\]$/,'$1'));
+        });
+        $('input[name=ds_order]')[0].value = order.join(',');
+        return true;
+    });
+    $('#stickerslist tr td input.position').hide();
+    $('#stickerslist tr td.handle').addClass('handler');
 });
