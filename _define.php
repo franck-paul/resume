039e7b12f581544611120641dff5e1169acbdf26
“philippe <philippe@dissitou.org>
2022-11-13 16:16:23 +0100

    Update _define.php

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     'Philippe aka amalgame and contributors',           // Author
     '2.5',                                            // Version
     [                                                   // Properties
-        'requires'          => [['core', '2.23']], 		        // Dependencies
+        'requires'          => [['core', '2.24']], 		        // Dependencies
         'standalone_config' => true,
         'type'              => 'theme',
         'tplset'            => 'dotty',
