b2427ce6fdbdf316f92ba2c32d7df45f3d36ed58
“philippe <philippe@dissitou.org>
2023-05-19 16:10:35 +0200

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -158,7 +158,7 @@ class Config extends dcNsProcess
                 // Template cache reset
                 dcCore::app()->emptyTemplatesCache();
 
-                dcPage::message(__('Theme configuration upgraded.'), true, true);
+                dcPage::success(__('Theme configuration upgraded.'), true, true);
             } catch (Exception $e) {
                 dcCore::app()->error->add($e->getMessage());
             }
