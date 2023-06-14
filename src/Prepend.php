9f3d717723a56919638e0d7c5a9dc7227653d94a
“philippe <philippe@dissitou.org>
2023-06-14 14:37:49 +0200

    use Dotclear\Module\MyTheme

--- src/Prepend.php
+++ src/Prepend.php
@@ -37,16 +37,11 @@ class Prepend extends dcNsProcess
                 return;
             }
 
-            if (preg_match('#^http(s)?://#', dcCore::app()->blog->settings->system->themes_url)) {
-                $theme_url = Http::concatURL(dcCore::app()->blog->settings->system->themes_url, '/' . dcCore::app()->blog->settings->system->theme);
-            } else {
-                $theme_url = Http::concatURL(dcCore::app()->blog->url, dcCore::app()->blog->settings->system->themes_url . '/' . dcCore::app()->blog->settings->system->theme);
-            }
-
-            echo '<script src="' . $theme_url . '/js/admin.js' . '"></script>' . "\n" .
-            '<script src="' . $theme_url . '/js/popup_media.js' . '"></script>' . "\n" .
+            echo 
+            My::jsLoad('admin.js') . "\n" .
+            My::jsLoad('popup_media.js') . "\n" .
             '<script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>' . "\n" .
-            '<link rel="stylesheet" media="screen" href="' . $theme_url . '/css/admin.css' . '" />' . "\n";
+            My::cssLoad('admin.css') . "\n" ;
 
             dcCore::app()->auth->user_prefs->addWorkspace('accessibility');
             if (!dcCore::app()->auth->user_prefs->accessibility->nodragdrop) {
