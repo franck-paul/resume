ed4559bbd62681eac847995a725131864a7edd9d
“philippe <philippe@dissitou.org>
2023-07-03 17:53:52 +0200

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -95,37 +95,6 @@ class Config extends Process
             return false;
         }
 
-        dcCore::app()->addBehavior('adminPageHTMLHead', function () {
-            if (dcCore::app()->blog->settings->system->theme !== My::id()) {
-                return;
-            }
-
-            echo 
-            My::jsLoad('admin.js') . "\n" .
-            My::jsLoad('popup_media.js') . "\n" .
-            '<script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>' . "\n" .
-            My::cssLoad('admin.css') . "\n" ;
-
-            dcCore::app()->auth->user_prefs->addWorkspace('accessibility');
-            if (!dcCore::app()->auth->user_prefs->accessibility->nodragdrop) {
-                echo
-                Page::jsLoad('js/jquery/jquery-ui.custom.js') .
-                Page::jsLoad('js/jquery/jquery.ui.touch-punch.js');
-            }
-        });
-
-        dcCore::app()->addBehavior('adminPageHTTPHeaderCSP', function ($csp) {
-            if (dcCore::app()->blog->settings->system->theme !== My::id()) {
-                return;
-            }
-
-            if (isset($csp['script-src'])) {
-                $csp['script-src'] .= ' use.fontawesome.com';
-            } else {
-                $csp['script-src'] = 'use.fontawesome.com';
-            }
-        });
-
         if (!empty($_POST)) {
             try {
                 // HTML
